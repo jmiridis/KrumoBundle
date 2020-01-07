8936b2e590ca23e5f8177c01d58dfb781e926606
jmiridis <jorgo@miridis.com>
2020-01-07 12:14:40 -0500

    Make Twig extensiong compatible with Twig2

--- Twig/Extension/TwigExtension.php
+++ Twig/Extension/TwigExtension.php
@@ -1,43 +1,52 @@
 <?php
-
+​
 namespace Oodle\KrumoBundle\Twig\Extension;
-
-class TwigExtension
-    extends \Twig_Extension
+​
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+​
+class TwigExtension extends AbstractExtension
 {
     public function setKrumoConfig($config)
     {
         require_once(__DIR__ . '/../../../../../krumo/class.krumo.php');
         \krumo::setConfig($config);
     }
-
+​
     public function getName()
     {
         return 'oodle_krumo_twig_extension';
     }
-
-    public function krumo($obj, $cascade = array())
+​
+    public function krumo($obj, $cascade = [])
     {
         ob_start();
         require_once(__DIR__ . '/../../../../../krumo/class.krumo.php');
-	if ($cascade)
+        if ($cascade) {
             \krumo::cascade($cascade);
-
+        }
+        ​
         krumo($obj);
-	if ($cascade)
+        if ($cascade) {
             \krumo::cascade(null);
-
+        }
+​
         return ob_get_clean();
     }
-
-
+​
+​
     public function getFilters()
     {
-        return array('krumo' => new \Twig_Filter_Method($this, 'krumo', array('is_safe' => array('html'))));
+        return [
+            new TwigFilter('krumo', [$this, 'krumo'], ['is_safe' => ['html']])
+        ];
     }
-
+​
     public function getFunctions()
     {
-        return array('krumo' => new \Twig_Function_Method($this, 'krumo', array('is_safe' => array('html'))));
+        return [
+            new TwigFunction('krumo', [$this, 'krumo'], ['is_safe' => ['html']])
+        ];
     }
 }
